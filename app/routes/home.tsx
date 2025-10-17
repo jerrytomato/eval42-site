cc1e10547484af42f35aea3364e83cb7719faa91
Liam Elliott <134457354+jerrytomato@users.noreply.github.com>
2025-10-17 16:38:02 -0400

    Add Header and Button Copy

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -5,7 +5,7 @@ import { WistiaPlayer } from "@wistia/wistia-player-react";
 
 import { getCalApi } from "@calcom/embed-react";
 
-export function meta({ }: Route.MetaArgs) {
+export function meta({}: Route.MetaArgs) {
   return [
     { title: "Sell More SaaS Without Bullshit" },
     { name: "description", content: "Demo page with video + booking" },
@@ -14,7 +14,7 @@ export function meta({ }: Route.MetaArgs) {
 
 export default function Home() {
   useEffect(() => {
-    (async function() {
+    (async function () {
       const cal = await getCalApi({ namespace: "grow" });
       cal("ui", { hideEventTypeDetails: false, layout: "month_view" });
     })();
@@ -22,10 +22,11 @@ export default function Home() {
 
   return (
     <div className="min-h-screen">
-      <main className="flex flex-col gap-10 mx-auto max-w-5xl">
-        <section className="text-center mt-10">
+      <main className="flex flex-col items-center gap-10 mx-auto max-w-5xl">
+        <section className="max-w-3xl text-center mt-10">
           <h1 className="text-3xl md:text-5xl font-black leading-tight">
-            Sell More SaaS Without Bullshit
+            The Smart Founder's Secret to Wild and{" "}
+            <span className="italic">Pain-Free</span> SaaS Growth
           </h1>
         </section>
 
@@ -45,14 +46,14 @@ export default function Home() {
             data-cal-config='{"layout":"month_view"}'
             className="btn-primary py-4 px-6"
           >
-            Start Growing for Free
+            Start Buying New Customers for Free
           </button>
         </section>
       </main>
 
       <footer className="mt-12 border-t">
         <div className="mx-auto w-full max-w-5xl px-6 py-8 text-sm">
-          © {new Date().getFullYear()} Eval 42
+          © {new Date().getFullYear()} Eval 42 Innovation Forward
         </div>
       </footer>
     </div>
